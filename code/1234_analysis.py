a7a2e8a0904f4f61e8b803a65b9d153d2c053966
Zhao-Ono <zhao@ono-pharma.com>
2025-12-09 15:17:16 +0900

    Update 1234_analysis.py

--- code/1234_analysis.py
+++ code/1234_analysis.py
@@ -10,9 +10,10 @@ Modified by Zhao Jinxuan
 if __name__ == '__main__':
     
     #import prerequisities
-    import argparse, importlib, importlib.util, sys, os
+    import argparse, importlib, importlib.util, sys, os, gc
     from utils.ProcessHandlers import Pipeline, FastqParser
     from utils.Dispatcher import Dispatcher
+    from utils.datatypes import Data
     
     #config file holds the information about library designs and
     #other parser instructions (where to look for data, where to save results etc)
@@ -34,21 +35,18 @@ if __name__ == '__main__':
     args = ap.parse_args()
 
     config = load_config(args.config)
-
-    dispatcher = Dispatcher(config)
-    pip, par  = dispatcher.dispatch_handlers((Pipeline, FastqParser))
-
     
     #initialize a dispatcher object; dispatcher is strictly speaking
     #not necessary, but it simplifies initialization of data handlers
-    dispatcher = Dispatcher(config)    
-    
+    dispatcher = Dispatcher(config)
+    pip, par  = dispatcher.dispatch_handlers((Pipeline, FastqParser))
+  
     #a list of handlers to initialize; pipeline should always be included
     #if NGS data parsing is the goal, FastqParser will do most of the work
-    handlers = (Pipeline, FastqParser)
+    #handlers = (Pipeline, FastqParser)
     
     #initialize the handlers
-    pip, par = dispatcher.dispatch_handlers(handlers)
+    #pip, par = dispatcher.dispatch_handlers(handlers)
         
     #enqueue the list of ops to run
     #note that at this stage no data processing will take place,
@@ -61,45 +59,34 @@ if __name__ == '__main__':
     #filters and summaries
     #note that only the filtered data will be passed to the next operation, think twice.
     pip.enque([
-                par.fetch_gz_from_dir(), 
-                par.revcom(),
-                #par.translate(stop_readthrough=False),
-                par.translate_all_frames(stop_readthrough=True), 
-                par.filt_ambiguous(where='dna'),
-                par.q_score_filt(minQ=20, loc=[1]),         
-                #par.len_summary(where='dna', save_txt=True),
-                #par.len_filter(where='pep'),
-                #par.cr_filter_fuzzy(where='dna', loc=[1], tol=9),             
-                par.cr_filter_fuzzy(where='pep', loc=[1], tol=3),
-                #par.vr_filter(where='pep', loc=[0], sets=[1, 2, 3]),
-                #par.mask_regions_fuzzy(where="dna", mode="vr", loc=[0,4]),
-                par.mask_regions_fuzzy(where="pep", mode="vr", loc=[0,4]),
-                #par.mask_regions_fuzzy(where="pep", mode="cr", loc=[1,3], tol=3),
+                par.translate_both_strands(stop_readthrough=False, utr5_offset=0), 
+                par.len_filter(where='dna', len_range=[800,1500]),
+                par.q_score_filt(minQ=30, frac=0.9),        
+                par.cr_filter_fuzzy(where='pep', loc=[2], tol=0),
+                par.fetch_at_fuzzy(where='pep', loc=[0,1,2], tol=0),
                 #par.convergence_summary(where='dna'),
-                par.convergence_summary(where='pep'),
-                #par.q_summary(loc='all', save_txt=True),
-                #par.fetch_at(where='pep', loc=[1, 2, 3]),
-                par.fetch_at_fuzzy(where='pep', loc=[1,2,3], tol=3),
+                #par.convergence_summary(where='pep'),
                 #par.freq_summary(where='dna', loc='all', save_txt=True),
-                par.freq_summary(where='pep', loc='all', save_txt=True),
+                #par.freq_summary(where='pep', loc='all', save_txt=True),
                 #par.count_summary(where='dna', top_n=500, fmt='csv'),
-                par.count_summary(where='pep', top_n=500, fmt='csv'),
+                par.count_summary(where='pep', fmt='csv'),
                 #par.count_summary(where='dna', top_n=500, fmt='fasta'),
-                par.count_summary(where='pep', top_n=500, fmt='fasta'),
-                #par.template_summary(where='dna'),
-                par.template_summary(where='pep'),
+                par.count_summary(where='pep', fmt='fasta'),
                 par.unpad(),
                 #par.save(where='dna', fmt='npy'),
                 par.save(where='pep', fmt='npy'),
                 #par.template_summary(where='dna'),
-                par.template_summary(where='pep'),
+                #par.template_summary(where='pep'),
                 #par.tSNE_analysis(where='pep', top_n=1000) #uncomment this line to run tSNE
               ])
 
-    #this will execute the pipeline
-    #if save_summary=True, summary will be saved in 
-    #the logs folder as specified in the config file
-    data = pip.run(save_summary=True)
+
+    data_iter = par.stream_chunks_from_gz_dir(chunk_lines=100000) 
+
+    pip.run_over_stream(data_iter, save_summary=True)
+    pip.merge_chunk_outputs(delete_chunks=True) 
+
+
         
         
     
